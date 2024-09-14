49d03e42731f7e09dec10fbbbb5bb5bbcab74b2e
Onofre <lonof.za@proton.me>
2024-09-13 22:40:28 -0700

    feat: métodos para mover celdas en x e y

--- malla_package/areas.py
+++ malla_package/areas.py
@@ -49,38 +49,71 @@ class Area:
         Actualiza el tamaño de la ventana con relacion a las nuevas proporciones de la malla
     """
     def __init__(self, malla, cel_inicio_x, cel_inicio_y, cel_fin_x, cel_fin_y):
+        self.malla = malla
         self.rango_x = [cel_inicio_x, cel_fin_x]
         self.rango_y = [cel_inicio_y, cel_fin_y]
-        self.inicio_x = 0
-        self.inicio_y = 0
-        self.fin_x = 0
-        self.fin_y = 0
-        self.ancho_x = 0
-        self.ancho_y = 0
-        self.__obtener_posicion_xy(malla)
-        self.__calcular_anchos()
-        self.ventana = self.obtener_ventana()
+        self.inicio_x = float
+        self.inicio_y = float
+        self.fin_x = float
+        self.fin_y = float
+        self.ancho_x = float
+        self.ancho_y = float
+        self.ventana = curses.window
+        self.__actualizar_posiciones()
+        self.__generar_ventana()
     
-    def __obtener_posicion_xy(self, malla):
-        self.inicio_x = int(self.__obtener_posicion(malla.celdas_x, self.rango_x[celda.INICIO], celda.INICIO))
-        self.inicio_y = int(self.__obtener_posicion(malla.celdas_y, self.rango_y[celda.INICIO], celda.INICIO))
-        self.fin_x = int(self.__obtener_posicion(malla.celdas_x, self.rango_x[celda.FIN], celda.FIN))
-        self.fin_y = int(self.__obtener_posicion(malla.celdas_y, self.rango_y[celda.FIN], celda.FIN))
-        
+    def __obtener_posicion_x(self):
+        self.inicio_x = int(self.__obtener_posicion(self.malla.celdas_x, self.rango_x[celda.INICIO], celda.INICIO))
+        self.fin_x = int(self.__obtener_posicion(self.malla.celdas_x, self.rango_x[celda.FIN], celda.FIN))
+
+    def __obtener_posicion_y(self):
+        self.inicio_y = int(self.__obtener_posicion(self.malla.celdas_y, self.rango_y[celda.INICIO], celda.INICIO))
+        self.fin_y = int(self.__obtener_posicion(self.malla.celdas_y, self.rango_y[celda.FIN], celda.FIN))
+
     def __obtener_posicion(self, celdas, celda, inicio_fin):
         return celdas[celda][inicio_fin]
     
     def __calcular_anchos(self):
         self.ancho_x = self.fin_x - self.inicio_x
         self.ancho_y = self.fin_y - self.inicio_y
-    
-    def obtener_ventana(self):
-        return curses.newwin(self.ancho_y, self.ancho_x, self.inicio_y, self.inicio_x)
         
+    def __actualizar_posiciones(self):
+        self.__obtener_posicion_x()
+        self.__obtener_posicion_y()
+        self.__calcular_anchos()
+            
+    def __generar_ventana(self):
+        self.ventana = curses.newwin(self.ancho_y, self.ancho_x, self.inicio_y, self.inicio_x)
+
+    def __redimensionar_celda(self):
+        try:
+            self.ventana.resize(self.ancho_y, self.ancho_x)
+            self.ventana.mvwin(self.inicio_y, self.inicio_x)
+        except:
+            self.__generar_ventana()
+        
+    def mover_celda_x(self, x):
+        distancia = x - self.rango_x[celda.INICIO]
+        celdas = len(self.malla.celdas_x) - 1
+        if (distancia <= celdas and distancia >= 0):
+            self.rango_x[celda.INICIO] = x
+            self.rango_x[celda.FIN] = self.rango_x[celda.FIN] + distancia if (self.rango_x[celda.FIN] + distancia) <= celdas else celdas
+            self.__actualizar_posiciones()
+            self.__redimensionar_celda()
+            
+    def mover_celda_y(self, y):
+        distancia = y - self.rango_y[celda.INICIO]
+        celdas = len(self.malla.celdas_y) - 1
+        if (distancia <= celdas and distancia >= 0):
+            self.rango_y[celda.INICIO] = y
+            self.rango_y[celda.FIN] = self.rango_y[celda.FIN] + distancia if self.rango_y[celda.FIN] + distancia <= celdas else celdas
+            self.__actualizar_posiciones()
+            self.__redimensionar_celda()
+            
     def refrescar(self):
         self.ventana.refresh()
     
     def actualizar_malla(self, malla):
-        self.__obtener_posicion_xy(malla)
-        self.__calcular_anchos()
-        self.ventana = self.obtener_ventana()
+        self.malla = malla
+        self.__actualizar_posiciones()
+        self.__generar_ventana()
